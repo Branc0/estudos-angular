caa385a6f8f06cb72e3635155d07f931de8d4fce
Branc0 <rrochadeazevedo@gmail.com>
2021-02-19 10:30:44 -0300

    excluindo testes gerados testes antigos, adicionando types do jasmine

--- alurapic/src/app/core/auth/auth.service.spec.ts
+++ alurapic/src/app/core/auth/auth.service.spec.ts
@@ -2,5 +2,8 @@ import { TestBed } from '@angular/core/testing';
 import { AuthService } from './auth.service';
 
 describe('Authservice', () => {
-  it('must be instantiated')
+
+  it('must be instantiated', () => {
+    expect(true).toBeTruthy();
+  });
 });
